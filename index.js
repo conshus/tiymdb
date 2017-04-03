4f32cd5850c0af2d67dd49e13881f29e8f1bc74e
Dwane <Dwane@hiphopandcode.com>
2017-04-02 22:17:54 -0400

    Goals 2 and 3 accomplished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Initialize variables
 let source = document.querySelector("#tiymdb-nowplaying-template").innerHTML;
 let template = Handlebars.compile(source);
 let source2 = document.querySelector("#tiymdb-actors-template").innerHTML;
@@ -7,14 +8,27 @@ let nowPlayingArrayCombined = [];
 let actorsArrayCombined = [];
 let actorBioAndFilmInfo = [];
 
+//Start
+getNowPlaying()
+  .then(object => object.results.map(getMovieInfo))
+  .then(moviePromises => Promise.all(moviePromises))
+  .then(object => object.map(addNowPlayingMovies))
+getNowPlaying()
+  .then(object => object.results.map(getMovieCredits))
+  .then(moviePromises => Promise.all(moviePromises))
+  .then(object => object.map(combineActors))
+  .then(placeInNowPlayingTemplate)
+
+//Section for adding Actor Info
 function placeInActorsTemplate(){
   console.log(actorBioAndFilmInfo)
   //let html = actorBioAndFilmInfo.map(actors => template2(actors)).join('');
   let html = template2(actorBioAndFilmInfo);
   let destination = document.querySelector('.tiymdb-actors');
   destination.innerHTML = html;
+  let closeToggle = document.querySelector("#close");
+  closeToggle.addEventListener("click", toggleSlider);
 }
-
 function combineActorInfo(actorInfo){
   console.log(actorInfo)
   actorBioAndFilmInfo = ({
@@ -26,25 +40,30 @@ function combineActorInfo(actorInfo){
   })
   placeInActorsTemplate();
 }
+function combineActorMovies(moviesInfo){
+  console.log(moviesInfo)
+  actorBioAndFilmInfo.movies = moviesInfo;
+  placeInActorsTemplate();
+}
 function getActorInfo(event){
-  //console.log(event)
   let actorPressed = event.target.dataset.actorid;
-  //console.log(actorPressed);
   fetch("https://api.themoviedb.org/3/person/"+actorPressed+"?api_key="+api_key+"&language=en-US&page=1")
     .then(actorInfo => actorInfo.json())
     .then(combineActorInfo)
-
   fetch("https://api.themoviedb.org/3/person/"+actorPressed+"/movie_credits?api_key="+api_key+"&language=en-US&page=1")
     .then(actorInfo => actorInfo.json())
-    //.then(combineActorInfo)
-    .then(object => console.log(object))
+    .then(combineActorMovies)
+    toggleSlider();
 }
+
+//Get the Movies Now Playing
 function getNowPlaying(){
   return fetch("https://api.themoviedb.org/3/movie/now_playing?api_key="+api_key+"&language=en-US&page=1")
   .then(nowPlayingInfo => nowPlayingInfo.json());
 }
+
+//Add movie info to a single array
 function addNowPlayingMovies(movieObject){
-  //console.log(movieObject)
   nowPlayingArrayCombined.push({
     title: movieObject.title,
     posterPic: baseImageUrl+movieObject.poster_path,
@@ -54,75 +73,53 @@ function addNowPlayingMovies(movieObject){
     runtime: movieObject.runtime,
     genre: movieObject.genres
   })
-  //console.log(nowPlayingArrayCombined)
 }
 function combineActors(castObject){
   actorsArrayCombined.push({cast: castObject.cast, crew: castObject.crew})
-  //console.log(actorsArrayCombined)
   addActors(actorsArrayCombined)
 }
 function addActors(actorsObject){
-  //console.log(castObject.cast)
-  //console.log(actorsObject)
-  //console.log(castObject.length)
   for (i=0; i<actorsObject.length; i++){
     nowPlayingArrayCombined[i].cast = actorsObject[i]
   }
-  //console.log(nowPlayingArrayCombined)
 }
-//function placeInNowPlayingTemplate(nowPlayingMovies){
 function placeInNowPlayingTemplate(){
-  //console.log(nowPlayingMovies)
-  //let html = nowPlayingMovies.results.map(movie => template(movie)).join('');
   let html = nowPlayingArrayCombined.map(movie => template(movie)).join('');
   let destination = document.querySelector('.tiymdb-nowplaying');
   destination.innerHTML = html;
-
   //Add event listeners for actor buttons
   let buttons = document.querySelectorAll(".actorButton");
   for (i=0; i<buttons.length; i++){
     let button = buttons[i];
     button.addEventListener("click", getActorInfo);
   }
-
-
 }
 
+//Get Movie Info
 function getInfo(url, params){
-  //Get Movie Info
-  //fetch("https://api.themoviedb.org/3/search/movie?api_key="+api_key+"&query="+movieTitle)
-  //console.log(url.id)
   return fetch("https://api.themoviedb.org/3/"+url+"?api_key="+api_key+"&"+params)
   .then(movieInfo => movieInfo.json());
 }
+
+//Get Movie Details
 function getMovieInfo(movieObject){
-  //console.log(movieObject);
   return getInfo("movie/"+movieObject.id);
-  //return getInfo(movieObject);
 }
+
+//Get Movie Cast and Crew
 function getMovieCredits(movieObject){
-  //console.log(movieObject);
   return getInfo("movie/"+movieObject.id+"/credits");
-  //return getInfo(movieObject);
 }
-//getNowPlaying()
-  //.then(object => object.results.map(addNowPlayingMovies))
-  //.then(placeInNowPlayingTemplate)
-getNowPlaying()
-  .then(object => object.results.map(getMovieInfo))
-  .then(moviePromises => Promise.all(moviePromises))
-  .then(object => object.map(addNowPlayingMovies))
-  //.then(object => console.log(object))
-getNowPlaying()
-  .then(object => object.results.map(getMovieCredits))
-  .then(moviePromises => Promise.all(moviePromises))
-  .then(object => object.map(combineActors))
-  .then(placeInNowPlayingTemplate)
-  //.then(object => console.log(object))
-  //.then(placeInNowPlayingTemplate)
-/*getNowPlaying()
-  .then(object => object.results.map(getMovieInfo))
-  .then(moviePromises => Promise.all(moviePromises))
-  .then(object => object.map(placeInActorsTemplate))
-  //.then(object => console.log(object))
-*/
+
+//Toggle for Actor Info Slider
+function toggleSlider(){
+  let slide = document.getElementById("slider").style;
+  if (slide.transform == "translateY(0%)"){
+    slide.transform = "translateY(100%)";
+    slide.display = "none";
+  }
+  else {
+    slide.transform = "translateY(0%)";
+    slide.display = "block";
+  }
+}
